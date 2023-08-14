ae41b6fa9b439261bd7fc74cd5708009894f6254
Victor Sadykov <svn1409@gmail.com>
2023-08-15 01:32:41 +0800

    updated

--- 15-1-3.py
+++ 15-1-3.py
@@ -1,20 +1,31 @@
 
 class Base:
+    price_class = 10
+    my_price = 0
+
     def price(self):
-        print(f'Вызов метода класса {self.__class__.__name__}')
-        return 10
+        print(f'{self.__class__.__dict__}')
+        print(f'{self.price_class}')
+        print(f'Вызов метода класса {self.__class__}')
+        return self.price_class
 
 
 class InterFoo(Base):
+    # price_class = 50
+
     def price(self):
-        print(f'Вызов метода класса {self.__class__.__name__}')
+        print(f'{self.__class__.__dict__}')
+        print(f'{self.price_class}')
+        print(f'Вызов метода класса {self.__class__}')
         return super().price() * 1.1
 
 
 class Discount(InterFoo):
+    # price_class = 100
     def price(self):
-        print(f'Вызов метода класса {self.__class__.__name__}')
-        return super().price() * 0.8
+        print(f'Вызов метода класса {self.__class__}')
+        print(f'{self.price_class}')
+        return super().price() * 0.8 + self.my_price
 
 
 print(Discount().price())
