02ae4b58ef82f027cdfe762820929a6fd562eb7a
Victor Sadykov <svn1409@gmail.com>
2023-08-21 08:06:29 +0800

    updated 16-1-2.py

--- 16-1-2.py
+++ 16-1-2.py
@@ -1,11 +1,26 @@
-try:
-    a, b = map(int, input().split())
-    result = a / b
-    print(result)
-except ZeroDivisionError as e:
-    print('деление на 0')
-    print(e.args)
-except ValueError as e:
-    print('ошибка типа')
-    print(e.args)
+print("\t\tНАЧАЛО программы\n")
+is_error = True
+while is_error:
+    try:
+        a, b = map(int, input('Введите 2 целых числа через пробел: ').split())
+        result = a / b
 
+    except ZeroDivisionError as temp_e:
+        print('деление на 0')
+        print(temp_e.args)
+
+    except TypeError as temp_e:
+        print('ошибка типа')
+        print(temp_e.args)
+
+    except Exception as temp_e:
+        print('Общая ошибка')
+        print(temp_e)
+
+    else:
+        is_error = False
+        print(f'Результат деления {a} на {b} = {result}')
+
+    finally:
+        if not is_error:
+            print("\t\tКОНЕЦ программы\n")
