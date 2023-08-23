427d74531f32bd9e4b986a39c027965ae935648d
VictorSadykov <svn1409@gmail.com>
2023-08-23 11:40:45 +0800

    updated 16-1 L - Принцип подстановки Барбары Лисков (Liskov Substitution)

--- Practica/16-2-1.py
+++ Practica/16-2-1.py
@@ -1,5 +1,6 @@
 # Описание задачи
-# 📮 Для существующего кода необходимо сделать рефакторинг с использованием принципов SOLID.
+#
+# Для существующего кода необходимо сделать рефакторинг с использованием принципов SOLID.
 # S - Принцип единой ответственности (Single Responsibility)
 #     Описание: каждый класс должен иметь только одну зону ответственности.
 #     Подход к реализации: выделите зоны ответственности в отдельные классы.
@@ -11,20 +12,14 @@
 #     Они могут определить, какие подклассы требуются,
 #     и усилить принцип единой ответственности, разделив обязанности кода.
 #
-#
+# L - Принцип подстановки Барбары Лисков (Liskov Substitution)
+#     Описание: если класс Parent является родительским классу Child,
+#     то любые методы класса Parent могут заменяться методами класса Child
+#     без возникновения ошибок в программе.
+#     Подход к реализации: используйте аргументы конструктора,
+#     чтобы обеспечить гибкость наследования.
 
 
-# Есть существующий код системы продаж, представленный классом Order (Заказ).
-#
-# В этом классе содержатся:
-# списки товаров в заказе (items)
-# количество каждого товара (quantities)
-# цена товара (prices)
-# статус заказа (status)
-# В классе Order есть методы для:
-# добавления товара в заказ (add_item)
-# подсчета стоимости заказа (total_price)
-# оплаты заказа (pay)
 
 from abc import ABC, abstractmethod
 
@@ -49,36 +44,43 @@ class Order:
   return '\n'.join(f'{item}, стоимостью {price} руб. в количестве {quantitie} шт.'
                    for item, quantitie, price in zip(self.items, self.quantities, self.prices))
 
- # def pay(self, payment_type, security_code):
- #  if payment_type.lower().strip() == "debit":
- #   print("Обработка дебетового типа платежа")
- #   print(f"Проверка кода безопасности: {security_code}")
- #   self.status = "paid"
- #  elif payment_type.lower().strip() == "credit":
- #   print("Обработка кредитного типа платежа")
- #   print(f"Проверка кода безопасности: {security_code}")
- #   self.status = "paid"
- #  else:
- #   raise Exception(f"Неизвестный способ оплаты: {payment_type}")
 
 class PaymentProcessor(ABC):
 
  @abstractmethod
- def pay(self, order, security_code):
+ def pay(self, order):
   pass
 
-
 class DebitPaymentProcessor(PaymentProcessor):
- def pay(self, order, security_code):
+
+ def __init__(self, security_code):
+  self.security_code = security_code
+
+ def pay(self, order):
   print("Обработка дебетового типа платежа")
-  print(f"Проверка кода безопасности: {security_code}")
+  print(f"Проверка кода безопасности: {self.security_code}")
   order.status = "paid"
 
 
 class CreditPaymentProcessor(PaymentProcessor):
- def pay(self, order, security_code):
+
+ def __init__(self, security_code):
+  self.security_code = security_code
+
+ def pay(self, order):
   print("Обработка кредитного типа платежа")
-  print(f"Проверка кода безопасности: {security_code}")
+  print(f"Проверка кода безопасности: {self.security_code}")
+  order.status = "paid"
+
+
+class PaypalPaymentProcessor(PaymentProcessor):
+
+ def __init__(self, email_address):
+  self.email_address = email_address
+
+ def pay(self, order):
+  print("Обработка paypal платежа")
+  print(f"Использование адреса электронной почты: {self.email_address}")
   order.status = "paid"
 
 
@@ -101,6 +103,10 @@ print(f'Общая сумма заказа = {order.total_price()} руб.')
 # except Exception as t_err:
 #  print(t_err)
 
-processor = PaymentProcessor()
-processor.pay_debit(order, "0372846")
-processor.pay_credit(order, "7383903")
+processor = DebitPaymentProcessor("0372846")
+processor.pay(order)
+processor = CreditPaymentProcessor("7383903")
+processor.pay(order)
+
+processor = PaypalPaymentProcessor("hi@company.com")
+processor.pay(order)
